a20bdc04fce967c7c8c1bb21a8f994917fea59d8
Shane Keney <shane@spikeball.com>
2019-12-10 21:13:49 -0500

    - main logic for generating password complete

--- script.js
+++ script.js
@@ -25,10 +25,78 @@ function generatePassword() {
         }
     }
 
+    let options = [];
+    numberCheck && options.push("number");
+    specialCharCheck && options.push("special");
+    lowercaseCheck && options.push("lowercase");
+    uppercaseCheck && options.push("uppercase");
+
     let generatedPassword = [];
+    let specialBool, lowercaseBool, uppercaseBool, numberBool;
 
     let specialCharacterString = `!"#$%&'()*+,-./:;<=>?@[\]^_{|}~`;
     let specialCharacterArray = specialCharacterString.split("");
+    let lettersArray = "qwertyuiopasdfghjklzxcvbnm".split("");
+    let numbersArray = "0123456789".split("");
 
-    
+    let min = 0;
+    let max = options.length - 1;
+    for(let i=0; i<parseInt(numberOfCharacters); i++) {
+        let random = Math.floor(Math.random() * (max - min + 1) + min);
+
+        switch(options[random]) {
+            case("number"):
+                let randomNumber = Math.floor(Math.random() * ((numbersArray.length - 1)  - min + 1) + min);
+                generatedPassword.push(numbersArray[randomNumber]);
+                numberBool = true;
+                break;
+            case("special"):
+                let randomSpecial = Math.floor(Math.random() * ((specialCharacterArray.length - 1)  - min + 1) + min);
+                generatedPassword.push(specialCharacterArray[randomSpecial]);
+                specialBool = true
+                break;
+            case("lowercase"):
+                let randomLetter = Math.floor(Math.random() * ((lettersArray.length - 1)  - min + 1) + min);
+                generatedPassword.push(lettersArray[randomLetter].toLowerCase());
+                lowercaseBool = true;
+                break;
+            case("uppercase"):
+                let randomLetterUpper = Math.floor(Math.random() * ((lettersArray.length - 1)  - min + 1) + min);
+                generatedPassword.push(lettersArray[randomLetterUpper].toUpperCase());
+                uppercaseBool = true;
+                break;
+            default:
+        }
+    }
+
+    let indexToAdd, itemToAdd;
+    if((specialBool && specialCharCheck) === false) {
+        indexToAdd = Math.floor(Math.random() * ((numberOfCharacters - 1)  - min + 1) + min);
+        itemToAdd = Math.floor(Math.random() * ((specialCharacterArray.length - 1)  - min + 1) + min);
+
+        generatedPassword[indexToAdd] = specialCharacterArray[itemToAdd];
+    }
+
+    if((uppercaseBool && uppercaseCheck) === false) {
+        indexToAdd = Math.floor(Math.random() * ((numberOfCharacters - 1)  - min + 1) + min);
+        itemToAdd = Math.floor(Math.random() * ((lettersArray.length - 1)  - min + 1) + min);
+
+        generatedPassword[indexToAdd] = lettersArray[itemToAdd].toUpperCase();
+    }
+
+    if((lowercaseBool && lowercaseCheck) === false) {
+        indexToAdd = Math.floor(Math.random() * ((numberOfCharacters - 1)  - min + 1) + min);
+        itemToAdd = Math.floor(Math.random() * ((lettersArray.length - 1)  - min + 1) + min);
+
+        generatedPassword[indexToAdd] = lettersArray[itemToAdd].toLowerCase();
+    }
+
+    if((numberBool && numberCheck) === false) {
+        indexToAdd = Math.floor(Math.random() * ((numberOfCharacters - 1)  - min + 1) + min);
+        itemToAdd = Math.floor(Math.random() * ((numbersArray.length - 1)  - min + 1) + min);
+
+        generatedPassword[indexToAdd] = numbersArray[itemToAdd];
+    }
+
+    console.log(generatedPassword.join(""));
 }
